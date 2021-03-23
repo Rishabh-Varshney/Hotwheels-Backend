67682b6ee980dc03c9858d50e71b483093cd98ce
Rishabh-Varshney <rishabh.varshney1124@gmail.com>
2021-03-24 02:51:03 +0530

    Configured config-service

--- src/app.module.ts
+++ src/app.module.ts
@@ -10,6 +10,7 @@ import { RestaurantsModule } from './restaurants/restaurants.module';
     ConfigModule.forRoot({
       isGlobal: true,
       envFilePath: process.env.NODE_ENV === 'dev' ? '.env.dev' : '.env.test',
+      ignoreEnvFile: process.env.NODE_ENV === 'prod',
     }),
     GraphQLModule.forRoot({
       autoSchemaFile: true,
@@ -17,11 +18,11 @@ import { RestaurantsModule } from './restaurants/restaurants.module';
     RestaurantsModule,
     TypeOrmModule.forRoot({
       type: 'postgres',
-      host: 'localhost',
-      port: 5432,
-      username: 'rv',
-      password: '12345',
-      database: 'hotwheels',
+      host: process.env.DB_HOST,
+      port: +process.env.DB_PORT,
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
       synchronize: true,
       logging: true,
     }),
