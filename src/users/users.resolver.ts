ec3334529ce7ebaabaf104c417831ca6443dac7f
Rishabh-Varshney <rishabh.varshney1124@gmail.com>
2021-03-29 08:41:13 +0530

    Created UpdateProfile-1

--- src/users/users.resolver.ts
+++ src/users/users.resolver.ts
@@ -4,6 +4,7 @@ import {
   CreateAccountInput,
   CreateAccountOutput,
 } from './dtos/create-account.dto';
+import { EditProfileInput, EditProfileOutput } from './dtos/edit-profile.dto';
 import { AuthGuard } from 'src/auth/auth.guard';
 import { LoginInput, LoginOutput } from './dtos/login.dto';
 import { User } from './entities/user.entity';
@@ -64,6 +65,22 @@ export class UserResolver {
       return {
         error: 'User Not Found',
         ok: false,
+      };
+    }
+  }
+
+  @UseGuards(AuthGuard)
+  @Mutation((returns) => EditProfileOutput)
+  async editProfile(
+    @AuthUser() authUser: User,
+    @Args('input') editProfileInput: EditProfileInput,
+  ): Promise<EditProfileOutput> {
+    try {
+      await this.usersService.editProfile(authUser.id, editProfileInput);
+    } catch (error) {
+      return {
+        ok: false,
+        error,
       };
     }
   }
