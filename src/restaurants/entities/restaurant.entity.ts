32cc074c9cb030627496093bfcb80aecf446afe7
Rishabh-Varshney <rishabh.varshney1124@gmail.com>
2021-04-03 18:32:44 +0530

    Edit Restaurant-2

--- src/restaurants/entities/restaurant.entity.ts
+++ src/restaurants/entities/restaurant.entity.ts
@@ -2,7 +2,7 @@ import { Field, InputType, ObjectType } from '@nestjs/graphql';
 import { IsString, Length } from 'class-validator';
 import { CoreEntity } from 'src/common/entities/core.entity';
 import { User } from 'src/users/entities/user.entity';
-import { Column, Entity, ManyToOne } from 'typeorm';
+import { Column, Entity, ManyToOne, RelationId } from 'typeorm';
 import { Category } from './category.entity';
 
 @InputType('RestaurantInputType', { isAbstract: true })
@@ -37,7 +37,10 @@ export class Restaurant extends CoreEntity {
   @ManyToOne(
     type => User,
     user => user.restaurants,
-    {onDelete: 'CASCADE'},
+    { onDelete: 'CASCADE' },
   )
   owner: User;
+
+  @RelationId((restaurant: Restaurant) => restaurant.owner)
+  ownerId: number;
 }
